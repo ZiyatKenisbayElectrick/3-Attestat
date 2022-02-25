f31b1d10a9a4314fa3e2cda3e2cfabf42fbdbe3a
nsemileevru <93764631+nsemileevru@users.noreply.github.com>
2022-02-25 18:24:44 +0300

    security

---
+++ Lab/laba3.py
@@ -0,0 +1,54 @@
+# Вариант 8 Линейный конгруэнтный а = 9, b = 5, m = 4096, Y0 = 502.
+
+a = 9
+b = 5
+m = 4096
+Y0 = 502
+
+def Gamma(y):
+    gamma_list = []
+    for _ in range(8):
+        y = (a * y + b) % m
+        gamma_list.append(y)
+    return gamma_list
+ 
+def Encryption():
+    gamma = Gamma(Y0)
+    res = open("Result.txt", "w", encoding="utf-8")
+    with open('Sourse.txt', 'r', encoding="utf-8") as f:
+        r_int = ""
+        r = ""
+        while True:
+            temp = f.read(8)
+            if temp:
+                for i, item in enumerate(temp):
+                    r_int = r_int + " " + str(ord(item) ^ gamma[i])
+                    r = r + " " + chr(ord(item) ^ gamma[i])
+                    res.write(chr(ord(item) ^ gamma[i]))
+ 
+            else:
+                break
+    print(r_int)
+    res.close()
+ 
+Encryption()
+ 
+def Decryption():
+    gamma = Gamma(Y0)
+    res = open("NewResult.txt", "w", encoding="utf-8")
+    with open('Result.txt', 'r', encoding="utf-8") as f:
+        r_int = ""
+        r = ""
+        while True:
+            temp = f.read(8)
+            if temp:
+                for i, item in enumerate(temp):
+                    r_int = r_int + " " + str(ord(item) ^ gamma[i])
+                    r = r + chr(ord(item) ^ gamma[i])
+                    res.write(chr(ord(item) ^ gamma[i]))
+            else:
+                break
+    print(r_int)
+    res.close()
+ 
+Decryption()
